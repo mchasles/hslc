e5b95c3fbe27e22d844735e2e4bc15b8b88529a1
Maxime Chasles <maxime.chasles@gmail.com>
2020-05-01 16:40:07 +0200

    book button box-shadow

--- src/components/BookButton.jsx
+++ src/components/BookButton.jsx
@@ -11,7 +11,7 @@ const Button = styled.a`
   text-align: center;
   line-height: 2.8em;
   background-color: rgb(165, 188, 84);
-  box-shadow: 0 0 4px 2px rgb(0, 0, 0, 0.4);
+  box-shadow: 0 0 8px 2px rgb(0, 0, 0, 0.2);
   color: white;
 
   text-transform: uppercase;
